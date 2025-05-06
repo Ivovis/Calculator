a6e80f27d33bc1473a9b956cdf55645a11cafd2e
Christopher Hunn <christopher.hunn@gmail.com>
2025-05-06 23:26:30 +0100

    feat: basic calculator - not working yet

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("I'm here!");
+
+// build string during
+function appendValue(value) {
+  document.getElementById("output").value += value;
+}
+
+function clearOutput() {
+  document.getElementById("output").value = "0";
+}
+
+function executeResult() {
+  inputStr = document.getElementById("output").value;
+
+  result = "to be done!";
+
+  document.getElementById("output").value = result;
+}
