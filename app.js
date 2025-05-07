3e618a4c2e8062a2dd4e1bbf533a6fba513fe73a
Christopher Hunn <christopher.hunn@gmail.com>
2025-05-07 07:04:45 +0100

    feat: fleshing out logic plans

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-console.log("I'm here!");
+// build output string during button presses
 
-// build string during
 function appendValue(value) {
-  document.getElementById("output").value += value;
+  // if value is a number 0 to 9
+  //    if the display only holds '0'
+  //        and the value is '0'
+  //            do nothing -> exit
+  //        or the value is '.'
+  //            update the display with '0.' -> exit
+  //     (its any non zero number)
+  //     replace the display value with the number
+  //  exit
+
+  // if we get to here we have other key (this will be expanded later)
+
+  switch (value) {
+    case "/":
+      console.log("we got /");
+      break;
+    case "*":
+      console.log("we got *");
+      break;
+    case "-":
+      console.log("we got -");
+      break;
+    case "+":
+      console.log("we got +");
+      break;
+    default:
+      console.log("we got an unexpected ", value, " it was unhandled");
+      break;
+  }
 }
 
 function clearOutput() {
